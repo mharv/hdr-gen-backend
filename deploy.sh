bf461763c189a339d84a9aa2b58f6288e9b0374d
mharv <mitch.k.harvey@gmail.com>
2022-03-02 15:58:46 +1100

    fixed deploy script to target correct image

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo az login 
-sudo az acr login --name hdrcontainers
+sudo az acr login --name hdrgencontainers
 
 sudo docker build -t hdr-gen .
-sudo docker tag openapi hdrcontainers.azurecr.io/hdr-gen:v1
-sudo docker push hdrcontainers.azurecr.io/hdr-gen:v1
+sudo docker tag hdr-gen hdrgencontainers.azurecr.io/hdr-gen:v2
+sudo docker push hdrgencontainers.azurecr.io/hdr-gen:v2
 
 
+# hdrcontainers.azurecr.io/hdr-gen hdrcontainers.azurecr.io/hdrgen hdrcontainers.azurecr.io/openapi hdrgencontainers.azurecr.io/hdr-gen aesopsustainability.azurecr.io/hdr-gen aesopsustainability.azurecr.io/hdr-gen
