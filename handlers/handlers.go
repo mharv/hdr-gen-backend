a6b9f0c890bf2f3754e28961b838692aa2a24b2c
mharv <mitch.k.harvey@gmail.com>
2022-03-09 23:35:42 +1100

    finished false color endpoint, did some cleanup, added some record exist checks in sql interactions

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -39,10 +39,32 @@ func PostProject(c *gin.Context) {
 	var project models.Project
 	c.BindJSON(&project)
 
-	if result := database.DB.Create(&project); result.Error != nil {
-		c.AbortWithStatus(http.StatusNotFound)
+	var exists bool
+	err := database.DB.Model(project).
+		Select("count(*) > 0").
+		Where("Number = ?", project.Number).
+		Find(&exists).
+		Error
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	if !exists {
+		fmt.Println("project record does not exist... saving now.")
+		if result := database.DB.Create(&project); result.Error != nil {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"message": result.Error.Error(),
+				"step":    "problem with db write",
+			})
+		} else {
+			c.JSON(http.StatusOK, project)
+		}
 	} else {
-		c.JSON(http.StatusOK, project)
+		fmt.Println("project record exists... doing nothing.")
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+			"message": fmt.Sprintf("Project with project number %s already exist", project.Number),
+		})
 	}
 }
 
@@ -180,14 +202,41 @@ func UpExposeImage(c *gin.Context) {
 	// upload result to storage with ACTIVE status
 	blobFileName := storage.UploadFileToBlobStore(imageName, fullPath+"pic/", false)
 
+	jpgblobFileName := storage.UploadFileToBlobStore(imageNameOnly+"-exposed.jpg", fullPath+"tif/", false)
+
+	// record metadata in sql
+	var image models.Image
+
+	image.ProjectId = int32(projectIdInt)
+	image.Name = jpgblobFileName
+	image.Type = "EXPOSED"
+	image.Status = "ACTIVE"
+
+	var exists bool
+	err = database.DB.Model(image).
+		Select("count(*) > 0").
+		Where("Name = ?", image.Name).
+		Find(&exists).
+		Error
+	if !exists {
+		fmt.Println("image record does not exist... saving now.")
+		if result := database.DB.Create(&image); result.Error != nil {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"message": err.Error(),
+				"step":    "problem with db write",
+			})
+			return
+		}
+	} else {
+		fmt.Println("image record exists... doing nothing.")
+	}
+
 	// get the exposed photo as a base64 encoded jpg and return in request
-	data, err := ioutil.ReadFile(fullPath + "tif/" + imageNameOnly + ".jpg")
+	data, err := ioutil.ReadFile(fullPath + "tif/" + imageNameOnly + "-exposed.jpg")
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-			"message": err.Error(),
-			"state":   "at read jpg",
+			"message": err,
 		})
-		return
 	}
 
 	var base64Encoding string
@@ -237,8 +286,37 @@ func DownExposeImage(c *gin.Context) {
 	// upload result to storage with ACTIVE status
 	blobFileName := storage.UploadFileToBlobStore(imageName, fullPath+"pic/", false)
 
+	jpgblobFileName := storage.UploadFileToBlobStore(imageNameOnly+"-exposed.jpg", fullPath+"tif/", false)
+
+	// record metadata in sql
+	var image models.Image
+
+	image.ProjectId = int32(projectIdInt)
+	image.Name = jpgblobFileName
+	image.Type = "EXPOSED"
+	image.Status = "ACTIVE"
+
+	var exists bool
+	err = database.DB.Model(image).
+		Select("count(*) > 0").
+		Where("Name = ?", image.Name).
+		Find(&exists).
+		Error
+	if !exists {
+		fmt.Println("image record does not exist... saving now.")
+		if result := database.DB.Create(&image); result.Error != nil {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"message": err.Error(),
+				"step":    "problem with db write",
+			})
+			return
+		}
+	} else {
+		fmt.Println("image record exists... doing nothing.")
+	}
+
 	// get the exposed photo as a base64 encoded jpg and return in request
-	data, err := ioutil.ReadFile(fullPath + "tif/" + imageNameOnly + ".jpg")
+	data, err := ioutil.ReadFile(fullPath + "tif/" + imageNameOnly + "-exposed.jpg")
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
 			"message": err,
@@ -258,13 +336,13 @@ func DownExposeImage(c *gin.Context) {
 
 func LuminanceMatrix(c *gin.Context) {
 	// upload bracketed set, create hdr, store to blob
-	// projectId := c.Params.ByName("projectId")
-	// projectIdInt, err := strconv.Atoi(projectId)
-	// if err != nil {
-	// 	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-	// 		"message": "Invalid projectId, must be an integer",
-	// 	})
-	// }
+	projectId := c.Params.ByName("projectId")
+	projectIdInt, err := strconv.Atoi(projectId)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid projectId, must be an integer",
+		})
+	}
 	// full image name including extension
 	imageName := c.Params.ByName("imageName")
 	extension := filepath.Ext(imageName)
@@ -292,8 +370,26 @@ func LuminanceMatrix(c *gin.Context) {
 	// deactivated for testing
 	blobFileName := storage.UploadFileToBlobStore(imageName, fullPath+"pic/", false)
 
+	jpgblobFileName := storage.UploadFileToBlobStore(imageNameOnly+"-scaled.jpg", fullPath+"tif/", false)
+
+	// record metadata in sql
+	var image models.Image
+
+	image.ProjectId = int32(projectIdInt)
+	image.Name = jpgblobFileName
+	image.Type = "SCALED"
+	image.Status = "ACTIVE"
+
+	if result := database.DB.Create(&image); result.Error != nil {
+		c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+			"message": err.Error(),
+			"step":    "problem with db write",
+		})
+		return
+	}
+
 	// get the exposed photo as a base64 encoded jpg and return in request
-	data, err := ioutil.ReadFile(fullPath + "tif/" + imageNameOnly + ".jpg")
+	data, err := ioutil.ReadFile(fullPath + "tif/" + imageNameOnly + "-scaled.jpg")
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
 			"message": err.Error(),
@@ -301,8 +397,6 @@ func LuminanceMatrix(c *gin.Context) {
 		})
 		return
 	}
-
-	// TODO do we record scling factor to mysql here?
 
 	var base64Encoding string
 
@@ -374,12 +468,15 @@ func ScaleImage(c *gin.Context) {
 	// deactivated for testing
 	blobFileName := storage.UploadFileToBlobStore(imageName, fullPath+"pic/", false)
 
+	jpgblobFileName := storage.UploadFileToBlobStore(imageNameOnly+"-scaled.jpg", fullPath+"tif/", false)
+	fmt.Println(jpgblobFileName)
+
 	// get the exposed photo as a base64 encoded jpg and return in request
-	data, err := ioutil.ReadFile(fullPath + "tif/" + imageNameOnly + ".jpg")
+	data, err := ioutil.ReadFile(fullPath + "tif/" + imageNameOnly + "-scaled.jpg")
 	if err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
 			"message": err.Error(),
-			"step":    "reading generated jpg",
+			"step":    "error reading generated jpg",
 		})
 		return
 	}
@@ -399,23 +496,55 @@ func ScaleImage(c *gin.Context) {
 
 func FalseColour(c *gin.Context) {
 	// upload bracketed set, create hdr, store to blob
-	// projectId := c.Params.ByName("projectId")
-	// projectIdInt, err := strconv.Atoi(projectId)
-	// if err != nil {
-	// 	c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
-	// 		"message": "Invalid projectId, must be an integer",
-	// 	})
-	// }
+	projectId := c.Params.ByName("projectId")
+	projectIdInt, err := strconv.Atoi(projectId)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid projectId, must be an integer",
+		})
+	}
 	// full image name including extension
-	// imageName := c.Params.ByName("imageName")
-	// extension := filepath.Ext(imageName)
-	// // full image name without extension
-	// imageNameOnly := strings.TrimSuffix(imageName, extension)
+	imageName := c.Params.ByName("imageName")
+	extension := filepath.Ext(imageName)
+	// full image name without extension
+	imageNameOnly := strings.TrimSuffix(imageName, extension)
 
-	// fullPath := createLocalWorkingDirectory(imageName)
+	fullPath := createLocalWorkingDirectory(imageName)
+
+	// load current HDR to tmp dir
+	storage.DownloadFileToLocalDir(imageName, fullPath+"pic/")
+
+	// run matrix script
+	out, err := exec.Command("./scripts/falsecolour.sh", imageNameOnly).Output()
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+			"step":    "falsecolour.sh",
+		})
+		return
+	}
+
+	fmt.Println(string(out))
+
+	// upload result to storage
+	// deactivated for testing
+	blobFileName := storage.UploadFileToBlobStore(imageNameOnly+"-falseColor.jpg", fullPath+"tif/", false)
+
+	// record metadata in sql
+	var image models.Image
+
+	image.ProjectId = int32(projectIdInt)
+	image.Name = blobFileName
+	image.Type = "FALSECOLOR"
+	image.Status = "ACTIVE"
+
+	if result := database.DB.Create(&image); result.Error != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"message": "placeholder",
+		"message": "false color generation complete.",
 	})
 }
 
